da3cb68674120a98c24b6531c6dd12949d77e3ac
Trym Gudvangen <trym.gudvangen@gmail.com>
2023-02-02 13:25:19 +0100

    feat: implement GoldGoal class

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/GoldGoal.java
@@ -0,0 +1,29 @@
+package edu.ntnu.idatt2001.group_30;
+
+/**
+ * This class represents a minimum gold threshold.
+ *
+ * @author Trym Hamer Gudvangen
+ */
+public class GoldGoal implements Goal{
+
+    private final int minimumGold;
+
+    /**
+     * The constructor defines the minimum amount of gold a given player can have.
+     * @param minimumGold    Minimum amount of gold, given as an int.
+     */
+    public GoldGoal(int minimumGold) {
+        this.minimumGold = minimumGold;
+    }
+
+    /**
+     * {@inheritDoc} gold.
+     * @param player    The player to be checked, given as a Player object.
+     * @return          Status of player, {@code true} if the player has enough gold, else {@code false}.
+     */
+    @Override
+    public boolean isFulfilled(Player player) {
+        return player.getGold() >= this.minimumGold;
+    }
+}
