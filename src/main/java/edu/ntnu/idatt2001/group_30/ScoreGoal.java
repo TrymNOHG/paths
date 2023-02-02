f180660f33ecf5da0b2af5307766889194db13cb
Trym Gudvangen <trym.gudvangen@gmail.com>
2023-02-02 13:21:26 +0100

    feat: implement ScoreGoal class

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/ScoreGoal.java
@@ -0,0 +1,29 @@
+package edu.ntnu.idatt2001.group_30;
+
+/**
+ * This class represents a minimum goal threshold.
+ *
+ * @author Trym Hamer Gudvangen
+ */
+public class ScoreGoal implements Goal{
+
+    private final int minimumPoints;
+
+    /**
+     * The constructor defines the minimum amount of points a given player can have.
+     * @param minimumPoints    Minimum amount of points, given as an int.
+     */
+    public ScoreGoal(int minimumPoints) {
+        this.minimumPoints = minimumPoints;
+    }
+
+    /**
+     * {@inheritDoc} score.
+     * @param player    The player to be checked, given as a Player object.
+     * @return          Status of player, {@code true} if the player has enough points, else {@code false}.
+     */
+    @Override
+    public boolean isFulfilled(Player player) {
+        return player.getScore() >= minimumPoints;
+    }
+}
