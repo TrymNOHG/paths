942cf8d6044ce322f49631234be4b9711a49c91c
Trym Gudvangen <trym.gudvangen@gmail.com>
2023-02-02 13:36:00 +0100

    feat: implement InventoryGoal class

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/InventoryGoal.java
@@ -0,0 +1,33 @@
+package edu.ntnu.idatt2001.group_30;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class represents the items that are expected in a player's inventory.
+ *
+ * @author Trym Hamer Gudvangen
+ */
+public class InventoryGoal implements Goal{
+
+    private final List<String> mandatoryItems;
+
+    /**
+     * The constructor defines the items a player must have.
+     * @param mandatoryItems    Expected items, given as a List{@code <String>}.
+     */
+    public InventoryGoal(List<String> mandatoryItems) {
+        this.mandatoryItems = mandatoryItems;
+    }
+
+    /**
+     * {@inheritDoc} inventory.
+     * @param player    The player to be checked, given as a Player object.
+     * @return          Status of player, {@code true} if the player has the items, else {@code false}.
+     */
+    @Override
+    public boolean isFulfilled(Player player) {
+        return player.getInventory().containsAll(mandatoryItems);
+        //TODO: Optimize ^^^ Maybe try the hashset constructor?
+    }
+}
