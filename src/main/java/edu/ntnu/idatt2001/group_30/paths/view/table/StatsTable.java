33e37f17d78f483515453223423ee1eac4038ca0
TrymHG <trymhg@stud.ntnu.no>
2023-04-15 14:57:19 +0200

    feat: add StatsTable

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/paths/view/table/StatsTable.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2001.group_30.paths.view.table;
+
+public class StatsTable<Player> extends TableDisplay<Player> {
+    /**
+     * This is a constructor which is used to construct a Player table.
+     *
+     * @param tableBuilder The builder used to construct a table, represented using an tableBuilder object.
+     */
+    public StatsTable(Builder<Player> tableBuilder) {
+        super(tableBuilder);
+    }
+}
