a8ae97a1083b5c9e5bb79630d3d3156349a66b35
TrymHG <trymhg@stud.ntnu.no>
2023-04-15 14:15:11 +0200

    feat: add specific GoalTable implementation

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/paths/view/table/GoalTable.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2001.group_30.paths.view.table;
+
+
+public class GoalTable<Goal> extends TableDisplay<Goal>{
+    /**
+     * This is a constructor which is used to construct a table for different goals.
+     *
+     * @param tableBuilder The builder used to construct a table, represented using an tableBuilder object.
+     */
+    public GoalTable(Builder<Goal> tableBuilder) {
+        super(tableBuilder);
+    }
+}
