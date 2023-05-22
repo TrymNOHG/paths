378b32a124e6dc00841f1d0bb1c908eca458566c
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2023-05-22 20:59:41 +0200

    feat: create general structure for pop ups

---
+++ src/main/java/edu/ntnu/idatt2001/group_30/paths/view/components/pop_up/AbstractPopUp.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2001.group_30.paths.view.components.pop_up;
+
+public abstract class AbstractPopUp {
+
+    protected void initialize() {
+        setupUiComponents();
+        setupBehavior();
+    }
+
+    protected abstract void setupUiComponents();
+
+
+    protected abstract void setupBehavior();
+
+    protected abstract void createPopUp();
+
+}
